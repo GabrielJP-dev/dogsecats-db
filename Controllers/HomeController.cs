ef8942a9c2e09eeb90d3ee076741f1172380fd85
Gabriel Paiva <1470350@sga.pucminas.br>
2024-06-14 19:44:28 -0300

    Adicionado conexão com Banco de Dados

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Entidades;
+using Infraestrutura;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dogsecats.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DogsController : ControllerBase
+    {
+        private DogseCatsDbContext _db;
+
+        public DogsController(DogseCatsDbContext db)
+        {
+            _db = db ?? throw new ArgumentNullException(nameof(db));
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var atendimentos = _db.Atendimentos.ToList();
+            return Ok(atendimentos);
+        }
+
+        [HttpPost]
+        public IActionResult Add(Atendimentos atendimento)
+        {
+            var atendimentos = _db.Atendimentos.Add(atendimento);
+            _db.SaveChanges();
+
+            return Ok(atendimentos.Entity);
+        }
+    }
+ }
